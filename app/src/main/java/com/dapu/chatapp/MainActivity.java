39cbc6afb81141f67b47cfba5ad0a3aae3a4a0a2
Matt-Parker99 <mattie2011.mp@gmail.com>
2020-01-22 18:32:41 +0000

    Small progress made on implementing a chat room but firebase needed

--- app/src/main/java/com/dapu/chatapp/MainActivity.java
+++ app/src/main/java/com/dapu/chatapp/MainActivity.java
@@ -4,11 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RoomListener{
+
+    private String channelID = "CHANNEL_ID_FROM_YOUR_SCALEDRONE_DASHBOARD";
+    private String roomName = "observable-room";
+    private EditText editText;
+    private Scaledrone scaledrone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
     }
+
+    @Override
+    public void onOpen(Room room) {
+        System.out.println("Connected to room");
+    }
+
+    @Override
+    public void onOpenFailure(Room room, Exception ex) {
+        System.err.println(ex);
+    }
+
+    @Override
+    public void onMessage(Room room, com.scaledrone.lib.Message receivedMessage) {
+        //TODO
+    }
+
 }
