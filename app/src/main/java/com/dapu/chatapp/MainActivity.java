0234912e0d31ebd04e819c728db812b01d879dc8
MichaelK972 <60009562+MichaelK972@users.noreply.github.com>
2020-02-06 15:01:28 +0000

    Commit of updated src directory
    Commit of updated src directory to solve missing symbol variable error.

--- app/src/main/java/com/dapu/chatapp/MainActivity.java
+++ app/src/main/java/com/dapu/chatapp/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.animation.ObjectAnimator;
+import android.os.Handler;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {//implements RoomListener{
 
@@ -16,14 +19,41 @@ public class MainActivity extends AppCompatActivity {//implements RoomListener{
     private Button signUp;
     //private Scaledrone scaledrone;
 
-    @Override
+
+
+        ProgressBar splashProgress;
+        int SPLASH_TIME = 2500; //This is 2.5 seconds
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.spash);
+
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //Do any action here. Now we are moving to next page
+                    //Intent mySuperIntent = new Intent(ActivitySplash.this, ActivityHome.class);
+                    //startActivity(mySuperIntent);
+                    setContentView(R.layout.activity_main);
+                    //This 'finish()' is for exiting the app when back button pressed from Home page which is ActivityHome
+                    //finish();
+
+                }
+            }, SPLASH_TIME);
+        }
+
+ /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //editText = (EditText) findViewById(R.id.editText);
         login = findViewById(R.id.login);
         signUp = findViewById(R.id.signUp);
-    }
+    } */
+
+
 
     public void Login(android.view.View view) {
         // Validate and sign in
