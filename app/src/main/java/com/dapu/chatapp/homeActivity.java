fed32c6610dd1e2759accdb0e22374f5b853cd80
MichaelK972 <60009562+MichaelK972@users.noreply.github.com>
2020-02-09 20:11:14 +0000

    Commit of src directory with firebase support.
    Commit of src directory and updated gradle build file to now support firebase and utilize firebase login and registration. Home page is now user specific.

--- app/src/main/java/com/dapu/chatapp/homeActivity.java
+++ app/src/main/java/com/dapu/chatapp/homeActivity.java
@@ -4,13 +4,32 @@ package com.dapu.chatapp;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.TextView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 public class homeActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private TextView email;
+    private TextView uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        uid = findViewById(R.id.uid);
+        email = findViewById(R.id.email);
+        if(user != null){
+            String email1 = user.getEmail();
+            String uid1 = user.getUid();
+            uid.setText(uid1);
+            email.setText(email1);
+        }
     }
 
 
