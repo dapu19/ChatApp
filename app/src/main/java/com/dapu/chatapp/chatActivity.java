9053773df04f14aeb358aca3c52a4ed28ea3a2d3
MichaelK972 <60009562+MichaelK972@users.noreply.github.com>
2020-03-02 21:09:15 +0000

    Fixed layout of chat rooms

--- app/src/main/java/com/dapu/chatapp/chatActivity.java
+++ app/src/main/java/com/dapu/chatapp/chatActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
@@ -18,6 +19,7 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -184,10 +186,14 @@ public class chatActivity extends AppCompatActivity{
 
         public void addMessageBox(String message, int type){
             TextView textView = new TextView(chatActivity.this);
-            textView.setTextSize(32);
+            textView.setTextSize(20);
             textView.setText(message);
-
-            LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            textView.setPadding(25,25,25,25);
+            LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            lp2.setMargins(20,10,20,10);
+            //textView.setLayoutParams(lp);
+            textView.setTextColor(Color.parseColor("#ffffff"));
+            //LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             lp2.weight = 7.0f;
 
             if(type == 2) {
